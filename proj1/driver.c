bd545233a37201ef880fa26a3fb943f0f5910c15
Jason Song <jasonqsong@outlook.com>
2016-02-19 16:43:49 -0500

    remove timer warnings

--- proj1/driver.c
+++ proj1/driver.c
@@ -15,18 +15,20 @@ Usage: ./driver
 #include <signal.h>
 #include <time.h>
 #include <linux/tcp.h>
+
 #define STARTTIMER 1
 #define ENDTIMER 0
 
 //packet with timer info
 struct packet{
-	int returnPort;
-        int seq;
-        double time;
-        int start; //start or cancel timer
-}pack;
+    int returnPort;
+    int seq;
+    double time;
+    int start; //start or cancel timer
+};
+struct packet pack;
 
-int port=3000;
+int port=8080;
 
 void starttimer(double time, int seq);
 void canceltimer(int seq);
@@ -36,51 +38,50 @@ struct sockaddr_in driver_addr;
 
 int main()
 {
-        //local driver socket
-        timer=socket(AF_INET, SOCK_DGRAM, 0); //create socket
-        if(timer<0){
-                printf("error opening socket\n");
-                exit(1);
-        }
+    //local driver socket
+    timer=socket(AF_INET, SOCK_DGRAM, 0); //create socket
+    if(timer<0){
+        perror("error opening socket\n");
+        exit(1);
+    }
 
-        //fill in tcpd socket's address information
-        driver_addr.sin_family=AF_INET;
-        driver_addr.sin_addr.s_addr=inet_addr("0");
-        driver_addr.sin_port=htons(port);
+    //fill in tcpd socket's address information
+    driver_addr.sin_family=AF_INET;
+    driver_addr.sin_port=htons(port);
 
 
-	starttimer(20.0,1);
-	starttimer(10.0,2);
-	starttimer(30.0,3);
-	sleep(5);
-	canceltimer(2);
-	starttimer(20.0,4);
-	sleep(5);
-	starttimer(18.0,5);
-	canceltimer(4);
-	canceltimer(8);
+    starttimer(20.0,1);
+    starttimer(10.0,2);
+    starttimer(30.0,3);
+    sleep(5);
+    canceltimer(2);
+    starttimer(20.0,4);
+    sleep(5);
+    starttimer(18.0,5);
+    canceltimer(4);
+    canceltimer(8);
 
-	close(timer);
- 	return 0;
+    close(timer);
+    return 0;
 }
 
 
 
 void starttimer(double time, int seq){
-	//set packet variables
-	pack.returnPort=port;
-	pack.seq=seq;
-	pack.time=time;
-	pack.start=STARTTIMER;
-	//send request to timer process
-	sendto(timer,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&driver_addr,sizeof(driver_addr));
+    //set packet variables
+    pack.returnPort=port;
+    pack.seq=seq;
+    pack.time=time;
+    pack.start=STARTTIMER;
+    //send request to timer process
+    sendto(timer,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&driver_addr,sizeof(driver_addr));
 }
 
 void canceltimer(int seq){
-	//set packet variables
-	pack.returnPort=port;
-	pack.seq=seq;
-	pack.start=ENDTIMER;
-	//send request to timer process
-	sendto(timer,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&driver_addr,sizeof(driver_addr));
-}
+    //set packet variables
+    pack.returnPort=port;
+    pack.seq=seq;
+    pack.start=ENDTIMER;
+    //send request to timer process
+    sendto(timer,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&driver_addr,sizeof(driver_addr));
+}  
--- proj1/timer.c
+++ proj1/timer.c
@@ -21,22 +21,22 @@ Usage: ./timer
 
 //node structure for list of packet timers
 struct node{
-	struct node *next;
-	struct node *prev;
-	double timeDiff;
-	int port;
-	int seq;
+    struct node *next;
+    struct node *prev;
+    double timeDiff;
+    int port;
+    int seq;
 };
 
 //packet with timer info 
 struct packet{
-        int returnPort;
-	int seq;
-	double time; 
-	int start; //start or cancel timer
+    int returnPort;
+    int seq;
+    double time; 
+    int start; //start or cancel timer
 }pack;
 
-int port=3000;
+int port=8080;
 
 int addNode(struct node **head);
 int deleteNode(struct node **head, int seq);
@@ -46,237 +46,235 @@ void printNodes(struct node *head);
 
 int main()
 {
-        int driver;
-        //local driver socket
-        driver=socket(AF_INET, SOCK_DGRAM, 0); //create socket
-        if(driver<0){
-                printf("error opening socket\n");
-                exit(1);
-        }
+    int driver;
+    //local driver socket
+    driver=socket(AF_INET, SOCK_DGRAM, 0); //create socket
+    if(driver<0){
+        printf("error opening socket\n");
+        exit(1);
+    }
 
-	//local socket information
-        struct sockaddr_in timer_addr;
-        timer_addr.sin_family=htons(AF_INET);
-        timer_addr.sin_addr.s_addr=inet_addr("0");
-        timer_addr.sin_port=htons(port);
-	struct sockaddr_in cli;
-	int len=sizeof(cli);
+    //local socket information
+    struct sockaddr_in timer_addr;
+    timer_addr.sin_family=AF_INET;
+    timer_addr.sin_addr.s_addr=INADDR_ANY;
+    timer_addr.sin_port=htons(port);
+    struct sockaddr_in cli;
+    int len=sizeof(cli);
 
         //bind tcpd socket
-        if(bind(driver,(struct sockaddr *)&timer_addr,sizeof(timer_addr))<0){
-                printf("error on local socket binding\n");
-                exit(1);
+    if(bind(driver,(struct sockaddr *)&timer_addr,sizeof(timer_addr))<0){
+        perror("error binding socket");
+        exit(1);
+    }
+
+    //initialize head of node to null
+    struct node *head;
+    head=NULL;
+
+    //initialize select values
+    fd_set fds;
+    struct timeval t;    
+    while(1){
+        //set fds values
+        FD_ZERO(&fds);
+        FD_SET(driver,&fds);
+        t.tv_sec=1;
+        t.tv_usec=0;
+        //call select with timeout looking at local socket
+        int rcv=select(driver+1,&fds,NULL,NULL,&t);
+        //if pack is in sock then read it
+        if(rcv>0){
+            recvfrom(driver,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&cli,&len);            
+            //if call for start of new timer then add node to list            
+            if(pack.start==STARTTIMER){
+                if(!addNode(&head))
+                    printf("error adding dup node %d\n",pack.seq);
+                else
+                    printf("added node %d, with timer %.02f\n",pack.seq,pack.time);
+            }
+            //if call to end timer delete node from list
+            if(pack.start==ENDTIMER){
+                if(!deleteNode(&head,pack.seq))
+                    printf("error on delete seq %d\n",pack.seq);
+                else
+                    printf("deleted node %d\n",pack.seq);
+            }
+
         }
-
-	//initialize head of node to null
-	struct node *head;
-	head=NULL;
-
-	//initialize select values
-	fd_set fds;
-	struct timeval t;	
-	while(1){
-		//set fds values
-		FD_ZERO(&fds);
-		FD_SET(driver,&fds);
-		t.tv_sec=1;
-		t.tv_usec=0;
-		//call select with timeout looking at local socket
-                int rcv=select(driver+1,&fds,NULL,NULL,&t);
-		//if pack is in sock then read it
-		if(rcv>0){
-			recvfrom(driver,(void *)&pack,sizeof(pack),0,(struct sockaddr *)&cli,&len);			
-			//if call for start of new timer then add node to list			
-			if(pack.start==STARTTIMER){
-				if(!addNode(&head))
-					printf("error adding dup node %d\n",pack.seq);
-				else
-					printf("added node %d, with timer %.02f\n",pack.time,pack.seq);
-			}
-			//if call to end timer delete node from list
-			if(pack.start==ENDTIMER){
-				if(!deleteNode(&head,pack.seq))
-					printf("error on delete seq %d\n",pack.seq);
-				else
-					printf("deleted node %d\n",pack.seq);
-			}
-			
-		}
-		//update timers on nodes
-		updateNodes(&head);	
-		//print current node list
-		printNodes(head);
-	}
-	//close sock
-	close(driver);
+        //update timers on nodes
+        updateNodes(&head);    
+        //print current node list
+        printNodes(head);
+    }
+    //close sock
+    close(driver);
 }
 
 
 //add node to list of seq# timers given pointer to head of node
 int addNode(struct node **head){
-	//create new node with given values
-	struct node *newNode;
-	newNode=(struct node *)malloc(sizeof(struct node));
-	newNode->timeDiff=pack.time;
-	newNode->port=pack.returnPort;
-	newNode->seq=pack.seq;
-	newNode->next=NULL;
-	newNode->prev=NULL;	
-	//temp node pointer to iterate through list
-	struct node *temp=*head;
-	
-	//if seq# is already in list return
-	if(checkForNode(*head, pack.seq)){
-		return 0;
-	}
-	//if list is empty make new node first node in list
-	if(*head == NULL){
-		*head=newNode;
-	}
-	//else if new node timer is less than start of list add to front of list
-	else if(temp->timeDiff > pack.time){
-		newNode->next=temp;
-		temp->prev=newNode;
-		*head=newNode;
-	}
-	//if not first element in list
- 	else{
-		//lower time of node as you iterate through list
-		newNode->timeDiff-=temp->timeDiff;
-		temp=temp->next;
-		int added=0;
-		//while in middle of list and new node hasnt been added
-		while(temp->next != NULL && added==0){
-			//add node to middle of list
-			if(newNode->timeDiff <= temp->timeDiff){
-				newNode->next=temp;
-				newNode->prev=temp->prev;
-				temp->prev->next=newNode;
-				temp->prev=newNode;
-				added=1;
-				break;
-			}
-			newNode->timeDiff-=temp->timeDiff;					
-			temp=temp->next;
-		}
-		//place new node at end of list
-		if(temp->next == NULL && temp->timeDiff < newNode->timeDiff && added==0){
-			newNode->prev=temp;
-			newNode->timeDiff-=temp->timeDiff;
-			temp->next=newNode;
-			added=1;
-		}
-		//place new node directly before last node in list
-		else if(temp->next == NULL && temp->timeDiff >= newNode->timeDiff && added==0){
-			newNode->next=temp;
-			newNode->prev=temp->prev;
-			temp->prev->next=newNode;
-			temp->prev=newNode;
-			added=1;
-		}
-	}
-	
-	//if theres another node after newNode get time difference
-	temp=newNode->next;
-	if(temp!=NULL)
-		temp->timeDiff-=newNode->timeDiff;
-	return 1;
+    //create new node with given values
+    struct node *newNode;
+    newNode=(struct node *)malloc(sizeof(struct node));
+    newNode->timeDiff=pack.time;
+    newNode->port=pack.returnPort;
+    newNode->seq=pack.seq;
+    newNode->next=NULL;
+    newNode->prev=NULL;    
+    //temp node pointer to iterate through list
+    struct node *temp=*head;
+
+    //if seq# is already in list return
+    if(checkForNode(*head, pack.seq)){
+        return 0;
+    }
+    //if list is empty make new node first node in list
+    if(*head == NULL){
+        *head=newNode;
+    }
+    //else if new node timer is less than start of list add to front of list
+    else if(temp->timeDiff > pack.time){
+        newNode->next=temp;
+        temp->prev=newNode;
+        *head=newNode;
+    }
+    //if not first element in list
+    else{
+        //lower time of node as you iterate through list
+        newNode->timeDiff-=temp->timeDiff;
+        temp=temp->next;
+        int added=0;
+        //while in middle of list and new node hasnt been added
+        while(temp->next != NULL && added==0){
+            //add node to middle of list
+            if(newNode->timeDiff <= temp->timeDiff){
+                newNode->next=temp;
+                newNode->prev=temp->prev;
+                temp->prev->next=newNode;
+                temp->prev=newNode;
+                added=1;
+                break;
+            }
+            newNode->timeDiff-=temp->timeDiff;                    
+            temp=temp->next;
+        }
+        //place new node at end of list
+        if(temp->next == NULL && temp->timeDiff < newNode->timeDiff && added==0){
+            newNode->prev=temp;
+            newNode->timeDiff-=temp->timeDiff;
+            temp->next=newNode;
+            added=1;
+        }
+        //place new node directly before last node in list
+        else if(temp->next == NULL && temp->timeDiff >= newNode->timeDiff && added==0){
+            newNode->next=temp;
+            newNode->prev=temp->prev;
+            temp->prev->next=newNode;
+            temp->prev=newNode;
+            added=1;
+        }
+    }
+
+    //if theres another node after newNode get time difference
+    temp=newNode->next;
+    if(temp!=NULL)
+        temp->timeDiff-=newNode->timeDiff;
+    return 1;
 }
 
 //delete node from list with given seq# and pointer to head of node
 int deleteNode(struct node **head, int seq){
-	int del=0;
-	struct node *temp=*head;
+    int del=0;
+    struct node *temp=*head;
 
-	//make sure list isnt empty
-	if(temp!=NULL){
-		//if first node is node to delete
-		if(temp->seq == seq){
-			//remove node and update next nodes timer
-			*head=temp->next;
-			if(temp->next!=NULL){
-				temp->next->prev=NULL;
-				temp->next->timeDiff+=temp->timeDiff;
-			}
-			
-			free(temp);
-			del=1;
-			
-		}
-		else{
-			//look through middle nodes
-			temp=temp->next;
-			while(temp->next!=NULL){
-				//remove node and update next nodes timer
-				if(temp->seq==seq){
-					if(temp->next!=NULL)
-						temp->next->timeDiff+=temp->timeDiff;
-					temp->prev->next=temp->next;
-					temp->next->prev=temp->prev;
-					del=1;
-					free(temp);
-					break;
-				}
-				
-			temp=temp->next;
-			}
-			//if last node
-			if(del==0 && temp->seq==seq){		
-				temp->prev->next=temp->next;
-				free(temp);
-				del=1;				
-			}	
-		}
-		
-	}
-	return del;
+    //make sure list isnt empty
+    if(temp!=NULL){
+        //if first node is node to delete
+        if(temp->seq == seq){
+            //remove node and update next nodes timer
+            *head=temp->next;
+            if(temp->next!=NULL){
+                temp->next->prev=NULL;
+                temp->next->timeDiff+=temp->timeDiff;
+            }
+            free(temp);
+            del=1;
+        }
+        else{
+            //look through middle nodes
+            temp=temp->next;
+            while(temp->next!=NULL){
+                //remove node and update next nodes timer
+                if(temp->seq==seq){
+                    if(temp->next!=NULL)
+                        temp->next->timeDiff+=temp->timeDiff;
+                    temp->prev->next=temp->next;
+                    temp->next->prev=temp->prev;
+                    del=1;
+                    free(temp);
+                    break;
+                }
+
+                temp=temp->next;
+            }
+            //if last node
+            if(del==0 && temp->seq==seq){        
+                temp->prev->next=temp->next;
+                free(temp);
+                del=1;                
+            }    
+        }
+
+    }
+    return del;
 }
 
 //find node with given seq number
 int checkForNode(struct node *head, int seq){
-	while(head != NULL){
-		if(head->seq == seq){
-			return 1;
-		}
-		head=head->next;
-	}
-	return 0;
+    while(head != NULL){
+        if(head->seq == seq){
+            return 1;
+        }
+        head=head->next;
+    }
+    return 0;
 }
 
 //update node timers
 void updateNodes(struct node **head){
-	struct node *temp=*head;
-	//get time difference
-	static time_t lasttime=0;
-	if(lasttime==0){
-		lasttime=time(NULL);
-	}
-	time_t nowtime=time(NULL);
-	uint64_t difftime=nowtime-lasttime;
-	lasttime=nowtime;
+    struct node *temp=*head;
+    //get time difference
+    static time_t lasttime=0;
+    if(lasttime==0){
+        lasttime=time(NULL);
+    }
+    time_t nowtime=time(NULL);
+    uint64_t difftime=nowtime-lasttime;
+    lasttime=nowtime;
 
-	//update first node timer and remove if <=0
-	if(temp != NULL){
-		temp->timeDiff-=difftime;
-		if(temp->timeDiff <= 0){
-			int seq=temp->seq;
-			//call back to driver here
-			if(deleteNode(head,seq))
-				printf("node %d expired\n",seq);
-		}
-		temp=temp->next;
-	}
+    //update first node timer and remove if <=0
+    if(temp != NULL){
+        temp->timeDiff-=difftime;
+        if(temp->timeDiff <= 0){
+            int seq=temp->seq;
+            //call back to driver here
+            if(deleteNode(head,seq))
+                printf("node %d expired\n",seq);
+        }
+        temp=temp->next;
+    }
 }
 
 //print node list
 void printNodes(struct node *head){
-	struct node *temp=head;
-	if(temp != NULL){
-		printf("Nodes: ");
-		while(temp != NULL){
-			printf("seq %d, time %.2f     ",temp->seq,temp->timeDiff);
-			temp=temp->next;
-		}
-		printf("\n");
-	}
+    struct node *temp=head;
+    if(temp != NULL){
+        printf("Nodes: ");
+        while(temp != NULL){
+            printf("seq %d, time %.2f     ",temp->seq,temp->timeDiff);
+            temp=temp->next;
+        }
+        printf("\n");
+    }
 }
